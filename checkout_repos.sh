d8cdef4ebbf6d3e736dc54ab4aede125b6161468
Jeff Maki <hello@jeffmaki.com>
2025-12-16 17:55:13 -0500

    Try #2

---
+++ checkout_repos.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# A script to prep the repos as the CI/CD pipeline does, e.g. when you're developing on your local machine
+#
+# Run as ./checkout-repos.sh <tag name> e.g. ./checkout-repos.sh dev
+#
+
+git clone --depth 1 --branch $1 https://github.com/TaskarCenterAtUW/workspaces-frontend.git frontend
+git clone --depth 1 --branch $1 https://github.com/TaskarCenterAtUW/workspaces-cgimap.git osm-cgimap
+git clone --depth 1 --branch $1 https://github.com/TaskarCenterAtUW/workspaces-openstreetmap-website.git osm-rails
+git clone --depth 1 --branch $1 https://github.com/TaskarCenterAtUW/workspaces-pathways-editor.git pathways-editor
+git clone --depth 1 --branch $1 https://github.com/TaskarCenterAtUW/workspaces-rapid.git rapid
+git clone --depth 1 --branch $1 https://github.com/TaskarCenterAtUW/workspaces-tasking-manager.git tasking-manager
+git clone --depth 1 --branch $1 https://github.com/TaskarCenterAtUW/workspaces-leaderboard.git leaderboard
---
+++ cleanup_repos.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -rf tasking-manager/
+rm -rf rapid/
+rm -rf pathways-editor/
+rm -rf osm-rails/
+rm -rf osm-cgimap/
+rm -rf leaderboard/
+rm -rf frontend/
